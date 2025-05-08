ee8c6beac769ff3b7913370ee986a48b777509d4
trabelsifiras <firas.trabelsi.1@esprit.tn>
2025-05-08 11:24:03 +0100

    add visitor info max,pcname

--- src/main/java/org/firas/portfolio/firasportfolio/controller/VisitorInfoController.java
+++ src/main/java/org/firas/portfolio/firasportfolio/controller/VisitorInfoController.java
@@ -1,11 +1,18 @@
 package org.firas.portfolio.firasportfolio.controller;
 
+import org.firas.portfolio.firasportfolio.data.PcInfo;
 import org.firas.portfolio.firasportfolio.data.VisitorInfo;
+import org.firas.portfolio.firasportfolio.repository.PcInfoRepository;
+import org.firas.portfolio.firasportfolio.repository.VisitorInfoRepository;
 import org.firas.portfolio.firasportfolio.service.VisitorInforService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/visitors")
@@ -14,7 +21,8 @@ public class VisitorInfoController {
 
     @Autowired
     private VisitorInforService service;
-
+    @Autowired
+    private PcInfoRepository repository;
     @GetMapping
     public List<VisitorInfo> getAll() {
         return service.getAll();
@@ -38,6 +46,35 @@ public class VisitorInfoController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
+    }
+
+    @GetMapping("/pcinfo")
+    public Map<String, String> getPcInfo() {
+        Map<String, String> info = new HashMap<>();
+        try {
+            String hostname = InetAddress.getLocalHost().getHostName();
+            InetAddress localHost = InetAddress.getLocalHost();
+            NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
+
+            StringBuilder macAddress = new StringBuilder();
+            if (ni != null && ni.getHardwareAddress() != null) {
+                byte[] mac = ni.getHardwareAddress();
+                for (int i = 0; i < mac.length; i++) {
+                    macAddress.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : ""));
+                }
+            } else {
+                macAddress.append("N/A");
+            }
+            PcInfo pcInfo = PcInfo.builder().pcName(hostname).macAddress(macAddress.toString()).build();
+            repository.save(pcInfo);
+            info.put("pcName", hostname);
+            info.put("macAddress", macAddress.toString());
+
+        } catch (Exception e) {
+            info.put("error", e.getMessage());
+        }
+
+        return info;
     }
 }
 
